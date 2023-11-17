440b6e0edf914719fcf0a7b31040f41693f79b5c
Seiya <20365512+seiyab@users.noreply.github.com>
2023-11-18 07:31:02 +0900

    add idempotency test (#3)

--- tests/format/index.test.ts
+++ tests/format/index.test.ts
@@ -6,10 +6,21 @@ import { format } from "prettier";
 describe("format", async () => {
 	const dir = join(import.meta.dir, "testdata");
 	const filenames = await readdir(dir);
+
 	test.each(filenames)("%s", async (name) => {
 		const path = join(dir, name);
 		const code = await readFile(path, "utf-8");
 		const result = await format(code, { filepath: path, plugins: ["."] });
 		expect(result).toMatchSnapshot();
+	});
+
+	describe("idempotency", () => {
+		test.each(filenames)("%s", async (name) => {
+			const path = join(dir, name);
+			const code = await readFile(path, "utf-8");
+			const result1 = await format(code, { filepath: path, plugins: ["."] });
+			const result2 = await format(result1, { filepath: path, plugins: ["."] });
+			expect(result1).toEqual(result2);
+		});
 	});
 });
