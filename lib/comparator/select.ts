1fa68d0689a1a707dc681a3adff7daf0cf18df2d
seiya <20365512+seiyab@users.noreply.github.com>
2023-11-17 21:37:33 +0900

    function property for type

--- lib/comparator/select.ts
+++ lib/comparator/select.ts
@@ -10,6 +10,9 @@ export const select = {
 	},
 	and,
 	or,
+	not<T>(predicate: (a: T) => boolean): (a: T) => boolean {
+		return (a) => !predicate(a);
+	},
 };
 
 function and<T, U extends T, V extends U>(
@@ -28,8 +31,25 @@ function and<T>(...predicates: ((a: T) => boolean)[]): (a: T) => boolean {
 function or<T, U extends T, V extends T>(
 	p1: (a: T) => a is U,
 	p2: (a: T) => a is V,
-	...predicates: ((a: T) => boolean)[]
 ): (a: T) => a is U | V;
+function or<T, U extends T, V extends T, W extends T>(
+	p1: (a: T) => a is U,
+	p2: (a: T) => a is V,
+	p3: (a: T) => a is W,
+): (a: T) => a is U | V | W;
+function or<T, U extends T, V extends T, W extends T, X extends T>(
+	p1: (a: T) => a is U,
+	p2: (a: T) => a is V,
+	p3: (a: T) => a is W,
+	p4: (a: T) => a is X,
+): (a: T) => a is U | V | W | X;
+function or<T, U extends T, V extends T, W extends T, X extends T, Y extends T>(
+	p1: (a: T) => a is U,
+	p2: (a: T) => a is V,
+	p3: (a: T) => a is W,
+	p4: (a: T) => a is X,
+	p5: (a: T) => a is Y,
+): (a: T) => a is U | V | W | X | Y;
 function or<T>(...predicates: ((a: T) => boolean)[]): (a: T) => boolean {
 	return (a) => predicates.some((predicate) => predicate(a));
 }
