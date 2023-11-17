17f5f53f84deed39e2fee43648c1f14320309881
seiya <20365512+seiyab@users.noreply.github.com>
2023-11-17 21:23:56 +0900

    type alias

---
+++ tests/format/testdata/type-alias.ts
@@ -0,0 +1,12 @@
+type A = {
+    z(): number;
+    y: number;
+    x: () => () => number;
+    new (a: 0, b: 1): A;
+    get w(): number;
+    v?(): number;
+    [u: number]: () => number;
+    [t: string]: unknown;
+    [`s`]: number;
+    r?: number;
+}
