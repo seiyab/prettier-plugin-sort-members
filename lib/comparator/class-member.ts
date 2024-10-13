38204d4bdad4b42f0ea2b3861273b45867511538
seiya <20365512+seiyab@users.noreply.github.com>
2024-10-14 08:13:05 +0900

    refactor

--- lib/comparator/class-member.ts
+++ lib/comparator/class-member.ts
@@ -1,13 +1,41 @@
 import { Node } from "../ast";
-import { abstracted } from "./abstracted";
 import { C, Comparator } from "./comparator";
-import { decorated } from "./decorated";
+import { MemberTypes } from "../ast/member-like";
 
 export function classMember(): Comparator<Node> {
 	return C.by(($) => {
 		if (decorated($)) return 2;
-		if ("static" in $ && $.static === true) return 1;
+		if (hasStaticModifier($)) return 1;
 		if (abstracted($)) return 4;
 		return 3;
 	}, C.number);
+}
+
+function decorated(node: Node): boolean {
+	switch (node.type) {
+		case MemberTypes.PropertyDefinition:
+		case MemberTypes.MethodDefinition:
+		case MemberTypes.ClassProperty:
+		case MemberTypes.ClassPrivateProperty:
+		case MemberTypes.ClassMethod:
+		case MemberTypes.ClassPrivateMethod:
+			return (node.decorators?.length ?? 0) > 0;
+	}
+	return false;
+}
+
+function abstracted(node: Node): boolean {
+	switch (node.type) {
+		case MemberTypes.TSAbstractPropertyDefinition:
+		case MemberTypes.TSAbstractMethodDefinition:
+			return true;
+		case MemberTypes.ClassProperty:
+		case MemberTypes.TSDeclareMethod:
+			return node.abstract === true;
+	}
+	return false;
+}
+
+function hasStaticModifier(node: Node): boolean {
+	return "static" in node && node.static === true;
 }
