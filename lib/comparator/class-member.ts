4583a736fc020d06718bb451e21babcdde15f6ea
Seiya <20365512+seiyab@users.noreply.github.com>
2024-10-14 08:41:16 +0900

    Improve compatibility with @typescript-eslint/member-ordering (#58)
    Issue: #57

--- lib/comparator/class-member.ts
+++ lib/comparator/class-member.ts
@@ -1,9 +1,41 @@
 import { Node } from "../ast";
 import { C, Comparator } from "./comparator";
+import { MemberTypes } from "../ast/member-like";
 
 export function classMember(): Comparator<Node> {
 	return C.by(($) => {
-		if ("static" in $) return $.static === true;
-		return false;
-	}, C.prefer);
+		if (decorated($)) return 2;
+		if (hasStaticModifier($)) return 1;
+		if (abstracted($)) return 4;
+		return 3;
+	}, C.number);
+}
+
+function decorated(node: Node): boolean {
+	switch (node.type) {
+		case MemberTypes.PropertyDefinition:
+		case MemberTypes.MethodDefinition:
+		case MemberTypes.ClassProperty:
+		case MemberTypes.ClassPrivateProperty:
+		case MemberTypes.ClassMethod:
+		case MemberTypes.ClassPrivateMethod:
+			return (node.decorators?.length ?? 0) > 0;
+	}
+	return false;
+}
+
+function abstracted(node: Node): boolean {
+	switch (node.type) {
+		case MemberTypes.TSAbstractPropertyDefinition:
+		case MemberTypes.TSAbstractMethodDefinition:
+			return true;
+		case MemberTypes.ClassProperty:
+		case MemberTypes.TSDeclareMethod:
+			return node.abstract === true;
+	}
+	return false;
+}
+
+function hasStaticModifier(node: Node): boolean {
+	return "static" in node && node.static === true;
 }
