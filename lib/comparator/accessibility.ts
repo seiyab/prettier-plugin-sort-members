c72d22da6e713900b8c3058f0ab6311e8dfeeb91
seiya <20365512+seiyab@users.noreply.github.com>
2024-10-26 07:53:34 +0900

    support accessor

--- lib/comparator/accessibility.ts
+++ lib/comparator/accessibility.ts
@@ -1,3 +1,4 @@
+import bt from "@babel/types";
 import { AST_NODE_TYPES } from "@typescript-eslint/types";
 import { C, Comparator } from "./comparator";
 import { MemberNode, MemberTypes } from "../ast/member-like";
@@ -17,11 +18,15 @@ export function accessibility<T extends MemberNode>(): Comparator<T> {
 		switch ($.type) {
 			case MemberTypes.PropertyDefinition:
 			case MemberTypes.MethodDefinition:
+			case MemberTypes.AccessorProperty:
 				if ($.key.type === AST_NODE_TYPES.PrivateIdentifier) return 3;
 				break;
 			case MemberTypes.ClassPrivateMethod:
 			case MemberTypes.ClassPrivateProperty:
 				return 3;
+			case MemberTypes.ClassAccessorProperty:
+				if (bt.isPrivateName($.key)) return 3;
+				break;
 		}
 		return 0;
 	}, C.number);
