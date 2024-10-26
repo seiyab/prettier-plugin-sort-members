d1c9bd4c027d7a7b8609e6effc75ffaf723886a1
seiya <20365512+seiyab@users.noreply.github.com>
2024-10-26 11:28:50 +0900

    move functions

--- lib/comparator/kind/utils.ts
+++
@@ -1,7 +0,0 @@
-import { MemberNode, MemberType } from "../../ast/member-like";
-
-export function node<K extends MemberType>(key: K) {
-	return function (node: MemberNode): node is MemberNode<K> {
-		return node.type === key;
-	};
-}
