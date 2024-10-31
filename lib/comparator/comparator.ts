0a93a4303bc9532eaa0a7e39fafbb2735867b6dc
Seiya <20365512+seiyab@users.noreply.github.com>
2024-11-01 07:29:21 +0900

    Refactor (#65)
    * remove unused comparators
    * refine capture signature
    * minor tweak

--- lib/comparator/comparator.ts
+++ lib/comparator/comparator.ts
@@ -7,88 +7,89 @@ export type Order = (typeof Order)[keyof typeof Order];
 
 export type Comparator<A> = (a: A, b: A) => number;
 
+export const C = {
+	by,
+	chain,
+	nop,
+	string,
+	number,
+	maybe,
+	capture,
+};
+
 function nop(this: void): Order {
 	return Order.Equal;
 }
 
-function capture<T, U extends T>(
+function by<T, U>(
 	this: void,
-	pred: (a: T) => a is U,
+	fn: ($: T) => U,
 	comp: Comparator<U>,
-): Comparator<T>;
-function capture<T>(this: void, pred: (a: T) => boolean): Comparator<T>;
-function capture<T, U extends T>(
-	this: void,
-	pred: (a: T) => boolean,
-	comp: Comparator<U> = nop,
 ): Comparator<T> {
 	return (a, b) => {
-		if (pred(a)) {
-			if (pred(b)) return comp(a as U, b as U);
-			return Order.Less;
+		return comp(fn(a), fn(b));
+	};
+}
+
+function chain<T>(this: void, ...comps: Comparator<T>[]): Comparator<T> {
+	return (a, b) => {
+		for (const comp of comps) {
+			const res = comp(a, b);
+			if (res !== Order.Equal) return res;
 		}
-		if (pred(b)) return Order.Greater;
 		return Order.Equal;
 	};
 }
 
-export const C = {
-	property<T, K extends keyof T>(
-		this: void,
-		key: K,
-		comp: Comparator<T[K]>,
-	): Comparator<T> {
+function string(this: void, a: string, b: string): Order {
+	if (a < b) return Order.Less;
+	if (a > b) return Order.Greater;
+	return Order.Equal;
+}
+
+function number(this: void, a: number, b: number): Order {
+	if (a < b) return Order.Less;
+	if (a > b) return Order.Greater;
+	return Order.Equal;
+}
+
+function maybe<T>(
+	this: void,
+	comp: Comparator<T>,
+): Comparator<T | undefined | null> {
+	return (a, b) => {
+		if (a == null) {
+			if (b == null) return Order.Equal;
+			return Order.Greater;
+		}
+		if (b == null) return Order.Less;
+		return comp(a, b);
+	};
+}
+
+type Captured<T, U> = Comparator<T> & {
+	then: (comp: Comparator<U>) => Comparator<T>;
+};
+
+function capture<T, U extends T>(
+	this: void,
+	pred: (a: T) => a is U,
+): Captured<T, U>;
+function capture<T>(this: void, pred: (a: T) => boolean): Comparator<T>;
+function capture<T, U extends T>(
+	this: void,
+	pred: (a: T) => boolean,
+): Captured<T, U> {
+	function then(comp: Comparator<U>): Comparator<T> {
 		return (a, b) => {
-			return comp(a[key], b[key]);
-		};
-	},
-	by<T, U>(this: void, fn: ($: T) => U, comp: Comparator<U>): Comparator<T> {
-		return (a, b) => {
-			return comp(fn(a), fn(b));
-		};
-	},
-	chain<T>(this: void, ...comps: Comparator<T>[]): Comparator<T> {
-		return (a, b) => {
-			for (const comp of comps) {
-				const res = comp(a, b);
-				if (res !== Order.Equal) return res;
-			}
+			if (pred(a) && pred(b)) return comp(a as U, b as U);
+			if (pred(a)) return Order.Less;
+			if (pred(b)) return Order.Greater;
 			return Order.Equal;
 		};
-	},
-	nop,
-	defer(this: void, a: boolean, b: boolean): Order {
-		if (!!a === !!b) return Order.Equal;
-		if (a) return Order.Greater;
-		return Order.Less;
-	},
-	prefer(this: void, a: boolean, b: boolean): Order {
-		if (Boolean(a) === Boolean(b)) return Order.Equal;
-		if (a) return Order.Less;
-		return Order.Greater;
-	},
-	string(this: void, a: string, b: string): Order {
-		if (a < b) return Order.Less;
-		if (a > b) return Order.Greater;
-		return Order.Equal;
-	},
-	number(this: void, a: number, b: number): Order {
-		if (a < b) return Order.Less;
-		if (a > b) return Order.Greater;
-		return Order.Equal;
-	},
-	maybe<T>(this: void, comp: Comparator<T>): Comparator<T | undefined | null> {
-		return (a, b) => {
-			if (a == null) {
-				if (b == null) return Order.Equal;
-				return Order.Greater;
-			}
-			if (b == null) return Order.Less;
-			return comp(a, b);
-		};
-	},
-	reverse<T>(this: void, comp: Comparator<T>): Comparator<T> {
-		return (a, b) => comp(b, a);
-	},
-	capture,
-};
+	}
+	const captured = then(nop) as Captured<T, U>;
+	captured.then = then;
+
+	return captured;
+}
