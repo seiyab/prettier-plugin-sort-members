43310cddd21f57524f0fb51b9f18f0f08f46daab
seiya <20365512+seiyab@users.noreply.github.com>
2024-11-03 22:54:34 +0900

    improve compatibility with @typescript-eslint/member-ordering { ordering: "alphabetically" }
    Issue: #66

--- lib/comparator/key-identifier-name.ts
+++ lib/comparator/key-identifier-name.ts
@@ -2,5 +2,4 @@ import { C, Comparator } from "./comparator";
 import { Node } from "../ast";
 import { nameOf } from "../ast/name-of";
 
-export const keyIdentifierName = (): Comparator<Node> =>
-	C.by(nameOf, C.maybe(C.string));
+export const keyIdentifierName = (): Comparator<Node> => C.by(nameOf, C.string);
