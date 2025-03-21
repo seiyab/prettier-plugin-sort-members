1f29f4e927e9051890511db23d49709f21cba80d
seiya <20365512+seiyab@users.noreply.github.com>
2025-03-21 18:55:05 +0900

    keep overloaded method from split

--- lib/comparator/class-member.ts
+++ lib/comparator/class-member.ts
@@ -1,6 +1,6 @@
 import { Node } from "../ast";
+import { abstracted, decorated, hasStaticModifier } from "../ast/class-member";
 import { C, Comparator } from "./comparator";
-import { MemberTypes } from "../ast/member-like";
 
 export function classMember(): Comparator<Node> {
 	return C.by(($) => {
@@ -9,35 +9,4 @@ export function classMember(): Comparator<Node> {
 		if (abstracted($)) return 4;
 		return 3;
 	}, C.number);
-}
-
-function decorated(node: Node): boolean {
-	switch (node.type) {
-		case MemberTypes.AccessorProperty:
-		case MemberTypes.PropertyDefinition:
-		case MemberTypes.MethodDefinition:
-		case MemberTypes.ClassAccessorProperty:
-		case MemberTypes.ClassProperty:
-		case MemberTypes.ClassPrivateProperty:
-		case MemberTypes.ClassMethod:
-		case MemberTypes.ClassPrivateMethod:
-			return (node.decorators?.length ?? 0) > 0;
-	}
-	return false;
-}
-
-function abstracted(node: Node): boolean {
-	switch (node.type) {
-		case MemberTypes.TSAbstractPropertyDefinition:
-		case MemberTypes.TSAbstractMethodDefinition:
-			return true;
-		case MemberTypes.ClassProperty:
-		case MemberTypes.TSDeclareMethod:
-			return node.abstract === true;
-	}
-	return false;
-}
-
-function hasStaticModifier(node: Node): boolean {
-	return "static" in node && node.static === true;
 }
