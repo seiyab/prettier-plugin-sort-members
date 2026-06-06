b18d7cbb54c4aee5c8d0efd1be3ee89661b84637
Seiya <20365512+seiyab@users.noreply.github.com>
2026-06-06 22:10:58 +0900

    update dependencies (#80)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,6 @@ import { languageOptions } from "./config/eslint/language-options.mjs";
 export default tseslint.config(
 	eslint.configs.recommended,
 	...tseslint.configs.recommendedTypeChecked,
-	// @ts-expect-error fixme: resolve dirname
 	{ languageOptions: languageOptions(import.meta.dirname) },
 	{ ignores: ["tests/**/testdata/", "dist/"] },
 	{ files: ["**/*.mjs"], ...tseslint.configs.disableTypeChecked },
